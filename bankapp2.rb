a96ce7d2e17c7a243a0a42bb77e3de7c1f5ed15b
Daniel Teale <djteale@gmail.com>
2018-08-24 09:31:12 +1000

    Overdraw prevention

--- bankapp2.rb
+++ bankapp2.rb
@@ -1,53 +1,47 @@
-balance = 0
+$balance = 0
 puts "Welcome to my shit bank"
 # Which action
 def whatdo
     puts "What would you like to do?"
     user_input = gets.chomp
-    user_input.downcase!
-    user_input
+    user_input.downcase
 end
 
 # Continue prompt
 def docontinue
     puts "Do you wish to continue banking?"
     user_input = gets.chomp
-    user_input.downcase!
-    user_input
+    user_input.downcase
 end
 loop do
     answer = whatdo
 # Action selector
     if answer == "balance"
-        puts "Your balance is $#{balance}"
+        puts "Your balance is $#{$balance}"
     end
-
+# Deposit function
     if answer == "deposit"
         puts "How much do you wish to deposit?"
         deposit = gets.chomp
-        balance = balance.to_f + deposit.to_f
-        puts "Your new balance is $#{balance}"
+        $balance = $balance.to_f + deposit.to_f
+        puts "Your new balance is $#{$balance}"
     end
-
+# Withdraw function
     if answer == "withdraw"
         puts "How much do you wish to withdraw?"
         withdraw = gets.chomp
-        if withdraw.to_f < balance.to_f
+        withdraw = withdraw.to_f
+        if withdraw > $balance
             puts "Insufficient funds"
+        else
+            $balance = $balance.to_f - withdraw.to_f
+            puts "Your new balance is $#{$balance}"   
         end
-        balance = balance.to_f - withdraw.to_f
-        puts "Your new balance is $#{balance}"
     end
-    
+# Continue prompt verifier   
     response = docontinue
     
-    if response != "yes" 
+    if response != "yes"
         break
     end
-
-    # if answer != "balance" || answer != "deposit"
-    #     puts "Invalid response"
-    #     break
-    # end
-
 end
