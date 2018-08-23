91ab08dc948e3465e5d2344fcd3e015aa0f5bb65
Daniel Teale <djteale@gmail.com>
2018-08-23 19:43:13 +1000

    Overdraw

--- bankapp.rb
+++ bankapp.rb
@@ -1,54 +1,54 @@
-#initialize
+# initialize
 balance = 0
 
-#input reader
-puts "Welcome to Shit bank"
-puts "What do you want to do?: "
-user_input = gets.chomp
-user_input.downcase!
-
-#Input verification
-if user_input == "balance"
+# Input reader
+def whatdo
+  puts 'Welcome to Shit bank'
+  puts 'What do you want to do?: '
+  user_input = gets.chomp
+  user_input.downcase!
+end
+whatdo
+# Input verification
+if user_input == 'balance'
   puts "Your balance is $#{balance}"
   puts "Would you like to take another action?"
   response = gets.chomp
   response.downcase!
-  while response == "yes"
-    puts "What do you want to do?: "
+  while response == 'yes'
+    puts 'What do you want to do?: '
     user_input = gets.chomp
     user_input.downcase!
   end
-elsif user_input == "deposit"
-  print "How much do you want to deposit?"
+elsif user_input == 'deposit'
+  print 'How much do you want to deposit?'
   deposit_amount = gets.chomp
   balance = balance.to_f + deposit_amount.to_f
   system('clear')
   puts "Your new balance is $#{balance}"
-  puts "Would you like to take another action?"
+  puts 'Would you like to take another action?'
   response = gets.chomp
   response.downcase!
-  while response == "yes"
-    puts "What do you want to do?: "
+  while response == 'yes'
+    puts 'What do you want to do?: '
     user_input = gets.chomp
     user_input.downcase!
   end
-elsif user_input == "withdraw"
-  puts "How much do you wish to withdraw?"
+elsif user_input == 'withdraw'
+  puts 'How much do you wish to withdraw?'
   withdraw_amount = gets.chomp
   balance = balance.to_f - withdraw_amount.to_f
   system('clear')
   puts "Your new balance is $#{balance}"
-  puts "Would you like to take another action?"
+  puts 'Would you like to take another action?'
   response = gets.chomp
   response.downcase!
-  while response == "yes"
-    puts "What do you want to do?: "
+  while response == 'yes'
+    puts 'What do you want to do?: '
     user_input = gets.chomp
     user_input.downcase!
-    if response != "yes"
-      break
-    end
+    break if response != 'yes'
   end
 else
-  puts "Invalid input"
+  puts 'Invalid input'
 end
--- bankapp2.rb
+++ bankapp2.rb
@@ -1,18 +1,53 @@
 balance = 0
+puts "Welcome to my shit bank"
+# Which action
 def whatdo
-    puts "Welcome to my shit bank"
     puts "What would you like to do?"
     user_input = gets.chomp
     user_input.downcase!
     user_input
 end
 
-answer = whatdo
-if answer == "balance"
-    puts "Your balance is $#{balance}"
-elsif answer == "deposit"
-    puts "How much do you wish to deposit?"
-    deposit = gets.chomp
-    balance = balance.to_f + deposit.to_f
-    puts "Your new balance is $#{balance}"
+# Continue prompt
+def docontinue
+    puts "Do you wish to continue banking?"
+    user_input = gets.chomp
+    user_input.downcase!
+    user_input
+end
+loop do
+    answer = whatdo
+# Action selector
+    if answer == "balance"
+        puts "Your balance is $#{balance}"
+    end
+
+    if answer == "deposit"
+        puts "How much do you wish to deposit?"
+        deposit = gets.chomp
+        balance = balance.to_f + deposit.to_f
+        puts "Your new balance is $#{balance}"
+    end
+
+    if answer == "withdraw"
+        puts "How much do you wish to withdraw?"
+        withdraw = gets.chomp
+        if withdraw.to_f < balance.to_f
+            puts "Insufficient funds"
+        end
+        balance = balance.to_f - withdraw.to_f
+        puts "Your new balance is $#{balance}"
+    end
+    
+    response = docontinue
+    
+    if response != "yes" 
+        break
+    end
+
+    # if answer != "balance" || answer != "deposit"
+    #     puts "Invalid response"
+    #     break
+    # end
+
 end
