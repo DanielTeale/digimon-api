7467137cf9acd7f5356101ea50ca32e46eb5f783
Daniel Teale <danielteale@daniels-air.ait-sydney.net.au>
2018-08-23 12:07:01 +1000

    First Copy

---
+++ bankapp.rb
@@ -0,0 +1,54 @@
+#initialize
+balance = 0
+
+#input reader
+puts "Welcome to Shit bank"
+puts "What do you want to do?: "
+user_input = gets.chomp
+user_input.downcase!
+
+#Input verification
+if user_input == "balance"
+  puts "Your balance is $#{balance}"
+  puts "Would you like to take another action?"
+  response = gets.chomp
+  response.downcase!
+  while response == "yes"
+    puts "What do you want to do?: "
+    user_input = gets.chomp
+    user_input.downcase!
+  end
+elsif user_input == "deposit"
+  print "How much do you want to deposit?"
+  deposit_amount = gets.chomp
+  balance = balance.to_f + deposit_amount.to_f
+  system('clear')
+  puts "Your new balance is $#{balance}"
+  puts "Would you like to take another action?"
+  response = gets.chomp
+  response.downcase!
+  while response == "yes"
+    puts "What do you want to do?: "
+    user_input = gets.chomp
+    user_input.downcase!
+  end
+elsif user_input == "withdraw"
+  puts "How much do you wish to withdraw?"
+  withdraw_amount = gets.chomp
+  balance = balance.to_f - withdraw_amount.to_f
+  system('clear')
+  puts "Your new balance is $#{balance}"
+  puts "Would you like to take another action?"
+  response = gets.chomp
+  response.downcase!
+  while response == "yes"
+    puts "What do you want to do?: "
+    user_input = gets.chomp
+    user_input.downcase!
+    if response != "yes"
+      break
+    end
+  end
+else
+  puts "Invalid input"
+end
