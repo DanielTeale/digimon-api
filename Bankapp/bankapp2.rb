31b40ded8b57d647f12f585b540f242368060bda
Daniel Teale <djteale@gmail.com>
2018-08-26 13:26:05 +1000

    integrated fetch system

---
+++ Bankapp/bankapp2.rb
@@ -0,0 +1,97 @@
+require_relative 'hashpractice'
+
+# Fetch balance data
+File.open("Bank_storage.txt", "r").each do |line|
+    $balance = (line.to_f)
+end
+
+
+$history = []
+puts "Welcome to my bank"
+
+
+# Fetch transaction history and store in array
+File.open("Balance_history.txt", "r").each do |line|
+    $history.push (line) 
+end
+
+# Which action
+def whatdo
+    puts "What would you like to do?"
+    user_input = gets.chomp
+    user_input.downcase
+end
+
+# Continue prompt
+def docontinue
+    puts "Do you wish to continue banking?"
+    user_input = gets.chomp
+    user_input.downcase
+end
+
+# Main Loop
+loop do
+    answer = whatdo
+
+# Action selector
+    if answer == "balance"
+        puts "Your balance is $#{$balance}"
+    end
+
+# Deposit function
+    if answer == "deposit"
+        puts "How much do you wish to deposit?"
+        deposit = gets.chomp
+        $balance = $balance.to_f + deposit.to_f
+        time = Time.now
+        $history.push ("#{time} " + "Balance: " + "$#{$balance} " + " | Transaction: " + "+$#{deposit}")
+        puts "Your new balance is $#{$balance}"
+    end
+
+# Withdraw function
+    if answer == "withdraw"
+        puts "How much do you wish to withdraw?"
+        withdraw = gets.chomp
+        withdraw = withdraw.to_f
+        if withdraw > $balance
+            puts "Insufficient funds"
+        else
+            $balance = $balance.to_f - withdraw.to_f
+            time = Time.now
+            $history.push ("#{time} " + "Balance: " + "$#{$balance} " + " | Transaction: " + "-$#{withdraw}")
+            puts "Your new balance is $#{$balance}"   
+        end
+    end
+
+# View history function
+    if answer == "history"
+        $history.each do |line|
+            puts line
+        end
+    end
+
+# Continue prompt verifier    
+    response = docontinue
+    if response != "yes"
+        break
+    else
+        system('clear')
+    end
+
+# Save balance   
+    File.open("Bank_storage.txt", "w") do |line|
+        line.puts "#{$balance}"
+    end
+end
+
+# Clear old transaction history
+File.open("Balance_history.txt", "w") do
+    print ""
+end
+
+# Save transaction history
+$history.each do |x|
+    File.open("Balance_history.txt", "a") do |line|
+        line.puts "#{x}"
+    end
+end
---
+++ Bankapp/hashpractice.rb
@@ -0,0 +1,27 @@
+require 'json'
+
+def to_json(hash)
+    JSON.generate hash
+end
+
+def from_json(hash)
+    JSON.parse hash
+end
+
+def save_file(path, data)
+    File.open(path, 'a') do |f|
+        f.puts to_json(data)
+    end
+end
+
+def read_file(path)
+    array = []
+    File.open(path, 'r') do |f|
+        f.each_line do |line|
+            hash = from_json(line)
+            array.push(hash)
+        end
+        f.close
+    end
+    array
+end
